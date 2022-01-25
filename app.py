f9f67929c149be7eb583a89d515ff18bd98fe639
JuliaByl <julia.bylund.97@gmail.com>
2022-01-25 21:12:07 +0000

    set up the rest of the pages with template from base.html

--- app.py
+++ app.py
@@ -10,5 +10,40 @@ if os.path.exists("env.py"):
     import env
 
 
-
 app = Flask(__name__)
+
+
+# home page
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+# browse recipes
+@app.route("/browse-recipes")
+def browse_recipes():
+    return render_template("browse-recipes.html")    
+
+
+# view recipe
+@app.route("/view-recipe")
+def view_recipe():
+    return render_template("view-recipe.html")
+
+
+# register
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+
+# log in
+@app.route("/login")
+def login():
+    return render_template("login.html")      
+
+
+# create recipe
+@app.route("/create-recipe")
+def create_recipe():
+    return render_template("create-recipe.html")   
