3099c7c646150e92730720658219eb1b3370b5c1
JuliaByl <julia.bylund.97@gmail.com>
2022-02-01 22:35:24 +0000

    create basic login function

--- app.py
+++ app.py
@@ -96,10 +96,30 @@ def register():
 
 
 # log in
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
-    return render_template("login.html")      
+    if request.method == "POST":
+        # if username exists in the database
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+        
+        if existing_user:  
+            # correct passord
+            if check_password_hash(
+                existing_user["password"], request.form.get("password")):
+                session["user"] = request.form.get("username").lower()
+                return redirect(url_for("home"))
+            # incorrect password
+            else:
+                flash("Incorrect password or username, try again!")
+                return redirect(url_for("login"))
+        # if ussername doesn't exist
+        else:
+            flash("Incorrect password or username, try again!")
+            return redirect(url_for("login"))        
 
+    return render_template("login.html")      
+        
 
 # create recipe
 @app.route("/create-recipe")
@@ -111,4 +131,3 @@ if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-            
